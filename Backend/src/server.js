dc8d0fe0bc326d6f2eb176fc45c2679198e4c928
H Lakshmish <lakshmish2001@gmail.com>
2026-08-05 09:40:49 +0530

    changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,10 +4,10 @@ const app = require("./app");
 const start = async () => {
     try {
         await app.listen({
-            port: 3000,
+            port: 5000,
         });
 
-        console.log("Server running on http://localhost:3000");
+        console.log("Server running on http://localhost:5000");
     } catch (err) {
         app.log.error(err);
         process.exit(1);
